3b53a054ec99cfef2f66cd218e3917f9522d339f
Saqer <ahmadsaqer95@outloo.com>
2021-04-28 18:11:14 +0200

    add the rest

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-import express from "express";
+const express = require('express');
+const errorHandler = require('./middlewares/error');
+require("dotenv").config();
 
 const app = express();
 
+
 app.get('/', (req, res) => res.send('hello oli and roby'));
+app.use(errorHandler);
 
 app.listen(9000, () => console.log("listening to port 9000"));
+
+console.log(process.env.PGUSER);
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
     console.error(err.stack)
-    res.status(500).send("Somethin got stupidly wrong here and somebody will get fired!!!")
+    res.status(500).send("Something got stupidly wrong here and somebody will get fired!!!")
 }
 
 module.exports = errorHandler;
